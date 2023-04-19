60842c47d5cccc4bacb6bccc443b00e7077dadbe
Quang Huy <saocungdc110249@gmail.com>
2023-04-19 18:20:08 +0700

    Add project files.

---
+++ BTVN/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Text;
+
+namespace BTVN
+{
+    class SinhVien
+    {
+        struct TTSV {
+            public string HoTen, GioiTinh, TenTruong, DiaChi, QueQuan, Gmail;
+            public DateTime Birth;
+
+        static void Main(string[] args)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+                Console.WriteLine("{0,55}", "***DANH SÁCH SINH VIÊN***");
+                Console.WriteLine("-Thông tin Sinh Viên 1: ");
+                Console.WriteLine("Họ Tên: Lê Hồng Nhung ");
+                Console.WriteLine("Giới tính: Nữ ");
+                Console.WriteLine("Ngày sinh: 25/08/2004 ");
+                Console.WriteLine("Tên trường đang theo học: NEU");
+                Console.WriteLine("Nơi ở: Chung cư hoàng huy");
+                Console.WriteLine("Quê quán: Thanh hóa");
+                Console.WriteLine("Địa chỉ email: hongnhungle491@gmail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+                Console.WriteLine("-Thông tin Sinh Viên 2: ");
+                Console.WriteLine("Họ Tên: Đặng Thành Long ");
+                Console.WriteLine("Giới tính: Nam");
+                Console.WriteLine("Ngày sinh: 16/07/1999");
+                Console.WriteLine("Tên trường đang theo học: HB Academi");
+                Console.WriteLine("Nơi ở: Hà Nội");
+                Console.WriteLine("Quê quán: Hà Nam");
+                Console.WriteLine("Địa chỉ email: skrillexvietnam160799@gmail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+                Console.WriteLine("-Thông tin Sinh Viên 3: ");
+                Console.WriteLine("Họ Tên: Nguyễn Triều Dương");
+                Console.WriteLine("Giới tính: Nam");
+                Console.WriteLine("Ngày sinh: 09/11/2004");
+                Console.WriteLine("Tên trường đang theo hoc: Greenwich");
+                Console.WriteLine("Nơi ở: Vinhomes Ocean Park");
+                Console.WriteLine("Quê quán Hà nội");
+                Console.WriteLine("Địa chỉ email: nguyentrieuduong0911@gmail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+                Console.WriteLine("-Thông tin Sinh Viên 4: ");
+                Console.WriteLine("Họ Tên: Nguyễn Công Nghĩa");
+                Console.WriteLine("Giới tính: Nam");
+                Console.WriteLine("Ngày sinh: 03/05/2002");
+                Console.WriteLine("Tên trường đang theo học: HB Academy");
+                Console.WriteLine("Nơi ở: Hà Nội");
+                Console.WriteLine("Quê quán: Hà Nội");
+                Console.WriteLine("Địa chỉ email: nghiapromali@gmail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+                Console.WriteLine("-Thông tin Sinh Viên 5: ");
+                Console.WriteLine("Họ Tên: Nguyễn Ngọc Châu");
+                Console.WriteLine("Giới tính: Nam");
+                Console.WriteLine("Ngày sinh: 17/01/2001");
+                Console.WriteLine("Tên trường đang theo học: Đại học Cần Thơ");
+                Console.WriteLine("Nơi ở: Cần Thơ");
+                Console.WriteLine("Quê quán: Sóc Trăng");
+                Console.WriteLine("Địa chỉ email: ngocchauststst@gmail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+                Console.WriteLine("-Thông tin Sinh Viên 5: ");
+                Console.WriteLine("Họ Tên: Bùi Quốc Bảo");
+                Console.WriteLine("Giới tính: Nam");
+                Console.WriteLine("Ngày sinh: 14/08/2004");
+                Console.WriteLine("Tên trường đang theo học: Đại học Bách KHoa Hà Nội");
+                Console.WriteLine("Nơi ở: Đống Đa");
+                Console.WriteLine("Quê quán: Thanh Hóa");
+                Console.WriteLine("Địa chỉ email: buibaomoyu@gmail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+                Console.WriteLine("-Thông tin Sinh Viên 6: ");
+                Console.WriteLine("Họ Tên: Hoàng Trung Khải");
+                Console.WriteLine("Giới tính: Nam");
+                Console.WriteLine("Ngày sinh: 09/10/2004");
+                Console.WriteLine("Tên trường đang theo học: Đại học Bách khoa");
+                Console.WriteLine("Nơi ở: Hoàng Mai, Hà Nội");
+                Console.WriteLine("Quê quán: Thanh Hóa");
+                Console.WriteLine("Địa chỉ email: khaihoang004@gmail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+                Console.WriteLine("-Thông tin Sinh Viên 7: ");
+                Console.WriteLine("Họ Tên: Lê Xuân Phúc");
+                Console.WriteLine("Giới tính: Nam");
+                Console.WriteLine("Ngày sinh: 17/02/2004");
+                Console.WriteLine("Tên trường đang theo học: Đại học Bách khoa Hà Nội");
+                Console.WriteLine("Nơi ở: Hà Nội");
+                Console.WriteLine("Quê quán: Thanh Hóa");
+                Console.WriteLine("Địa chỉ email: xuanphuctls@gmail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+                Console.WriteLine("-Thông tin Sinh Viên 8: ");
+                Console.WriteLine("Họ Tên: Nguyễn Trần Minh");
+                Console.WriteLine("Giới tính: Nam");
+                Console.WriteLine("Ngày sinh: 25/03/2003");
+                Console.WriteLine("Tên trường đang theo học: ĐH Bách Khoa Hà Nội");
+                Console.WriteLine("Nơi ở: 250 Minh Khai");
+                Console.WriteLine("Quê quán: Thái Bình");
+                Console.WriteLine("Địa chỉ email: dreamcat553@gmail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+                Console.WriteLine("-Thông tin Sinh Viên 9:");
+                Console.WriteLine("Họ Tên: Lê đình hùng anh");
+                Console.WriteLine("Giới tính: Nam");
+                Console.WriteLine("Ngày sinh: 27/04/2023");
+                Console.WriteLine("Tên trường đang theo học: Lam sơn");
+                Console.WriteLine("Nơi ở: ktx ĐHBK");
+                Console.WriteLine("Quê quán: Thanh Hóa");
+                Console.WriteLine("Địa chỉ email: ledinhhunganh1@gmail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+                Console.WriteLine("-Thông tin Sinh Viên 10: ");
+                Console.WriteLine("Họ Tên: Trần Minh Đức");
+                Console.WriteLine("Giới tính: Nam");
+                Console.WriteLine("Ngày sinh: 13/02/2002");
+                Console.WriteLine("Tên trường đang theo học: đh kinh doanh và công nghệ");
+                Console.WriteLine("Nơi ở: 17 nam dư");
+                Console.WriteLine("Quê quán: hải dương");
+                Console.WriteLine("Địa chỉ email: MinhDucc13022002@gmail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+                Console.WriteLine("-Thông tin Sinh Viên 11: ");
+                Console.WriteLine("Họ Tên: Vũ Đức Nam");
+                Console.WriteLine("Giới tính: Nam");
+                Console.WriteLine("Ngày sinh: 10/05/2003");
+                Console.WriteLine("Tên trường đang theo học: Cao Đẳng FPT Polytechnic");
+                Console.WriteLine("Nơi ở: Hà Nội");
+                Console.WriteLine("Quê quán: Hải Dương");
+                Console.WriteLine("Địa chỉ email: clone@gMail.com");
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+            }
+        }
+    }
+}
